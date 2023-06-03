95dee28ea7d59785f8d7e3f12fdb3b02fc425a59
Maurizio Branca <maurizio.branca@gmail.com>
2023-06-03 09:46:17 +0200

    Index a document
    To finishing this first hacking session, we'll read a JSON
    document from stdin and index it using an `IndexRequest`.
    
    refs: #1

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"log"
 	"os"
 	"strings"
 
 	elasticsearch "github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
 )
 
 func main() {
+
+	//
+	// Get the Elasticsearch client settings from the environment variables.
+	//
 
 	endpoints, ok := os.LookupEnv("ELASTICSEARCH_ENDPOINTS")
 	if !ok {
@@ -22,6 +29,10 @@ func main() {
 		os.Exit(1)
 	}
 
+	//
+	// Create the Elasticsearch client.
+	//
+
 	cfg := elasticsearch.Config{
 		Addresses: strings.Split(endpoints, ","),
 		APIKey:    apiKey,
@@ -33,6 +44,41 @@ func main() {
 		os.Exit(1)
 	}
 
+	//
+	// Get the Elasticsearch client info and cluster info.
+	//
+
 	log.Println(elasticsearch.Version)
-	log.Println(client.Info())
+
+	info, err := client.Info()
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+		os.Exit(1)
+	}
+	defer info.Body.Close()
+	log.Println(info)
+
+	//
+	// Index a document in Elasticsearch from stdin.
+	//
+
+	// Read from stdin until a newline is encountered.
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	log.Println(text)
+
+	// Build the request.
+	req := esapi.IndexRequest{
+		Index: "test",
+		Body:  strings.NewReader(text),
+	}
+
+	// Perform the request with the client.
+	res, err := req.Do(context.Background(), client)
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+		os.Exit(1)
+	}
+
+	log.Println(res)
 }
